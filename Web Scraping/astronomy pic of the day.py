89a654b3249fb2653376a0c012aa9fc2e56e903c
thelegend2742069 <pranavshirole.pvt@gmail.com>
2022-08-23 23:59:10 +0530

    added relative dates (today, yesterday)

--- Web Scraping/astronomy pic of the day.py
+++ Web Scraping/astronomy pic of the day.py
@@ -1,7 +1,9 @@
-import requests, os, bs4, re
+import requests, os, bs4, re, datetime
 from PIL import Image
 
 def main(var):
+    if var == 'today':
+        var = re.sub('-','', str(datetime.date.today())[2:])
     res = requests.get('https://apod.nasa.gov/apod/ap%s.html' %(var))
     res.raise_for_status
 
@@ -13,7 +15,7 @@ def main(var):
     imgres = requests.get(imgURL)
     imgres.raise_for_status
     
-
+    print('downloading image...\n\n')
     imgFile = open('Astronomy Pic of the Day/AstroPic %s.jpg' %(var), 'wb')
     for chunk in imgres.iter_content(100000):
         imgFile.write(chunk)
@@ -28,6 +30,14 @@ def main(var):
     print('\n\n'+explan.split(' Tomorrow')[0])
 
 os.makedirs('Astronomy Pic of the Day', exist_ok=True)
-date = input("Enter a date in YYMMDD format (after 1995 June 20): ")
+date = input("Enter a date (after 1995 June 20) in YYMMDD format ('today' and 'yesterday' are also fine): ")
+
+if date == 'today':
+    date = re.sub('-','', str(datetime.date.today())[2:])
+
+elif date == 'yesterday':
+    date = re.sub('-','', str(datetime.date.today())[2:])
+    date = str(int(date)-1)
+
 main(date)
 
